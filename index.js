efa287415f5424a86217000b5a8993f598fe707e
Jason <castrob.jason@gmail.com>
2023-03-22 19:19:41 -0600

    fist commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hola Jason")
+console.log("Hola Jason Castro Blanco")
+
+var a = 50;
+var b = 30;
+
+function suma(a,b)  {
+ result = a + b
+ return result
+
+}
+
+console.log(suma(a,b))
+
+
+/** */
